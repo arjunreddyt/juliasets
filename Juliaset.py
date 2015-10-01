551cee0d2df76f51afabb3f8bb9b2ac98126ec21
Arjun Reddy Tummala <Tumma101@mail.chapman.edu>
2015-10-01 04:01:27 +0000

    hw4

---
+++ Juliaset.py
@@ -0,0 +1,34 @@
+class JuliaSet(object):
+    
+    def __init__(self, c, n=100):
+        self.c = c
+        self.n = n
+        self._d = 0.001
+        self._complexplane = []
+        self.set = []
+       
+    def makeplane(self):
+        r=[i*self._d for i in range(int(-2/self._d),int(2/self._d),1)]
+        self._complexplane=[complex(x,y) for x in r for y in r]
+        
+    def juliamap(self, z):
+        return (z**2) + self.c
+    
+    def iterate(self, z):
+        m = 0
+        while True:
+            z=self.juliamap(z)
+            m = m + 1
+            if  abs(z) > 2:
+                return m
+            if m >=self.n:
+                return 0
+
+    def set_spacing(self, d):
+        self._d = d
+        self.makeplane()
+
+    def generate(self):
+        self.set = [self.iterate(z) for z in self._complexplane]
+        return self.set
+
